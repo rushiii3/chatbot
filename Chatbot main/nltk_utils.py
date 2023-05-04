e080fca5b453fc74c5a17edcb12d41481335a6e7
rushiii3 <105168088+rushiii3@users.noreply.github.com>
2023-05-04 10:04:04 +0000

    light

--- Chatbot main/nltk_utils.py
+++ Chatbot main/nltk_utils.py
@@ -1,4 +1,12 @@
 import nltk
 from nltk.stem.porter import PorterStemmer
 stemmer = PorterStemmer()
-nltk.download('punkt')
+#nltk.download('punkt')
+def tokenize(sentence):
+    return nltk.word_tokenize(sentence)
+def stem(word):
+    return stemmer.stem(word)
+
+words = ["program", "programs", "programmer", "programming", "programmers"]
+print(stem)
+#print(tokenize("Hello how are you"))
