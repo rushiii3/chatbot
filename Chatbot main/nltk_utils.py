d2704d7e93eed3a336178a08646deaf4c4a31435
rushiii3 <105168088+rushiii3@users.noreply.github.com>
2023-05-07 16:52:21 +0000

    trained

--- Chatbot main/nltk_utils.py
+++ Chatbot main/nltk_utils.py
@@ -1,13 +1,16 @@
 import nltk
 from nltk.stem.porter import PorterStemmer
+import numpy as np
 stemmer = PorterStemmer()
 #nltk.download('punkt')
 def tokenize(sentence):
     return nltk.word_tokenize(sentence)
 def stem(word):
     return stemmer.stem(word.lower())
-
-words = ["program", "programs", "programmer", "programming", "programmers"]
-b = [stem(w) for w in words]
-print(b)
-print(tokenize("Hello how are you"))
+def bag_of_words(tokenized_sentence,words):
+    stemed = [stem(words) for words in tokenized_sentence]
+    bag = np.zeros(len(words),dtype=np.float32)
+    for idx,w in enumerate(words):
+        if( w in stemed ):
+            bag[idx] = 1
+    return bag
