ea9a7dfa73a1ea62fdc918859635a37f51c121b5
rushiii3 <105168088+rushiii3@users.noreply.github.com>
2023-05-07 12:48:58 +0000

    stemming

--- Chatbot main/nltk_utils.py
+++ Chatbot main/nltk_utils.py
@@ -5,8 +5,9 @@ stemmer = PorterStemmer()
 def tokenize(sentence):
     return nltk.word_tokenize(sentence)
 def stem(word):
-    return stemmer.stem(word)
+    return stemmer.stem(word.lower())
 
 words = ["program", "programs", "programmer", "programming", "programmers"]
-print(stem(words))
+b = [stem(w) for w in words]
+print(b)
 #print(tokenize("Hello how are you"))
