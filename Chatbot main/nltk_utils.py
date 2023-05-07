e5b2d3e5e208350ade936ab3399951b429ec47f8
rushiii3 <105168088+rushiii3@users.noreply.github.com>
2023-05-07 12:50:11 +0000

    ignore

--- Chatbot main/nltk_utils.py
+++ Chatbot main/nltk_utils.py
@@ -10,4 +10,4 @@ def stem(word):
 words = ["program", "programs", "programmer", "programming", "programmers"]
 b = [stem(w) for w in words]
 print(b)
-#print(tokenize("Hello how are you"))
+print(tokenize("Hello how are you"))
