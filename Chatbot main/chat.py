4fefa86faa8a638ed1d8be3d6209910e05c78524
rushiii3 <105168088+rushiii3@users.noreply.github.com>
2023-05-17 07:15:34 +0000

    comit thiss

--- Chatbot main/chat.py
+++ Chatbot main/chat.py
@@ -1,46 +1,72 @@
+import random
 import json
-from nltk_utils import tokenize,bag_of_words
 import torch
 from model import NeuralNet
-import random
+from nltk_utils import bag_of_words, tokenize
+
+
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
-with open("intents.json","r") as f:
-    intents = json.load(f)
+
+
+samples = "1. If you wish to compare our systems model side by side filtering to your specifications, visit <a href = 'https://digicor.com.au/advanced-search'> DiGiCOR Applicator </a> . <br> 2. If you wish to obtain pricing, simply ask to speak to a live agent"
+
+
+with open('intents.json', 'r') as json_data:
+    intents = json.load(json_data)
 
 FILE = "data.pth"
 data = torch.load(FILE)
+
+
+
+
 input_size = data["input_size"]
 hidden_size = data["hidden_size"]
 output_size = data["output_size"]
 pattern_all_words = data["pattern_all_words"]
-tags = data["tags"]
+tags = data['tags']
 model_state = data["model_state"]
-model = NeuralNet(input_size,hidden_size,output_size).to(device)
+
+
+model = NeuralNet(input_size, hidden_size, output_size).to(device)
 model.load_state_dict(model_state)
 model.eval()
-bot_name = "ChatBot"
-print("Lets chat! (Type 'quit' to exit)")
+
+bot_name = "DiGiCOR Chatbot" 
+
+
+
 def get_response(msg):
+    sentence = tokenize(msg)
+    X = bag_of_words(sentence, pattern_all_words)
+    X = X.reshape(1, X.shape[0])
+    X = torch.from_numpy(X).to(device)
+
+    output = model(X)
+    _, predicted = torch.max(output, dim=1)
+
+    tag = tags[predicted.item()]
+ 
+    probs = torch.softmax(output, dim=1)
+    prob = probs[0][predicted.item()]
+    if prob.item() > 0.85:      #Increasing specifisity to reduce incorrect classifications
+        for intent in intents['intents']:
+            if tag == intent["tag"]:
+                return random.choice(intent['responses'])
+    
+    return f"I'm sorry, but I cannot understand your query. {samples} "
+
+
+
+
+if __name__ == "__main__":
+    print("Let's chat! (type 'quit' to exit)")
     while True:
-        sentence = input(f":")
+        # sentence = "do you use credit cards?"
+        sentence = input("You: ")
         if sentence == "quit":
-            print("Happy to talk to you!!")
             break
-        sentence = tokenize(sentence)
-        x = bag_of_words(sentence,pattern_all_words)
-        x = x.reshape(1,x.shape[0])
-        x = torch.from_numpy(x).to(device)
-        output = model(x)
-        _, predicted = torch.max(output,dim=1)
-        tag = tags[predicted.item()]
-        probs = torch.softmax(output,dim=1)
-        prob = probs[0][predicted.item()]
-        if prob.item()>0.75:
-            for intent in intents["intents"]:
-                if tag==intent["tag"]:
-                    print(f"{bot_name}:{random.choice(intent['responses'])}")
-        else:
-            print(f"{bot_name}:I dont... understand")
 
-
+        resp = get_response(sentence)
+        print(resp)
 
